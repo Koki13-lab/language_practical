f4f07a56c9f26510b972af341a6b08f6e2a11dde
Koki13-lab <itatyan17_130811@yahoo.co.jp>
2026-02-08 14:31:30 +0900

    java commit

---
+++ java/src/SeventhProgram.java
@@ -0,0 +1,17 @@
+package src;
+
+public class SeventhProgram {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+		int num1 = Integer.parseInt(args[0]);
+		int num2 = Integer.parseInt(args[1]);
+		int result = num1 + num2;
+		
+		System.out.println("一つ目の引数 : " + num1);
+		System.out.println("二つ目の引数 : " + num2);
+		System.out.println("加算の結果 : " + result);
+	}
+
+}
---
+++ java/src/SixthProgram.java
@@ -0,0 +1,13 @@
+package src;
+
+public class SixthProgram {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+		int num1 = 2;
+		String num2 = "12";
+		System.out.println(num1 + num2);
+	}
+
+}
